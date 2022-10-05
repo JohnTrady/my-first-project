a77835d412a82c9e9b44f6a0411172214c736de9
John Trady <docktor.mom19.93@gmail.com>
2022-10-05 19:37:56 +0300

    lesson-1

--- js/app.js
+++ js/app.js
@@ -1,39 +1,4 @@
-// const pi = 3.14;
-// const surfaceOfMars = 4 * pi * 3397 * 3397;
-// const surfaceOFMercury = 4 * pi * 2440 * 2440;
 
 
-// console.log(surfaceOFMercury);
-// console.log(surfaceOfMars);
-
-
-// const pi = 3.14;
-
-// const surfaceAreaCalculator = (raduis) => {
-//     return 4 * pi * raduis * raduis;   // опредление функции 
-// }
-
-// const surfaceOfMars = surfaceAreaCalculator(3390);  // вызов функции
-// const surfaceOFMercury  = surfaceAreaCalculator(2440);
-
-// console.log (surfaceOfMars);
-// console.log(surfaceOFMercury);
-
-/*---------- */
-
-// const percentageCalculator = (number, total) => {
-//     return number * 100 / total;
-// }
-
-// console.log (percentageCalculator(80, 160));
-
-// a ** 2 + 2 * a * b + b ** 2
-
-function sumOFSquares(a, b) {
-
-  return (a ** 2) + (2 * a * b) + (b ** 2);
-
-}
-
-console.log(sumOFSquares(77, 48));
-
+// git clone src
+// 
