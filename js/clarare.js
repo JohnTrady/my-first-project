44dbf95895d6be99122deb39628515f12900e72d
John Trady <docktor.mom19.93@gmail.com>
2022-10-17 20:00:00 +0300

    Закончили Lesson-3
    расмотрели переменные и строки , разобрали   декларативное и императивное программирование.

--- js/clarare.js
+++ js/clarare.js
@@ -29,24 +29,75 @@
 
 // console.log(factorial(3));
 
-const smallestDivisor = n => {
-    if (n <= 1) {
-    return "Our number shoud be  more than 1"
-     }
+// const smallestDivisor = n => {
+//     if (n <= 0) {
+//     return "Our number shoud be  more than 0"
+//      }
+
+//      if (n === 1) {
+//         return 1;
+//      }
         
-     if (n%2 === 0 && n > 1) {
-          return 2;
-  }
-
-    let divider = 2;
+//      if (n%2 === 0) {
+//           return 2;
+//   }
 
 
-    while (n%divider !==0) {
-        divider = divider  + 1;
-    }
 
-    return divider;
+//     let divider = 2;
+
+     
+  
+
+
+//     while (n%divider !==0 && divider !== 1) {
+//         divider = divider  + 1;
+//     }
+
+//     return divider + "-это наш наименьший делитель";
     
-};
+// };
 
-console.log(smallestDivisor(35));
+// console.log(smallestDivisor(49));
+
+
+//   Строки 
+//   строки с которыми ведется работа  в js  объявляются с помощью одинарных ковычек '';
+//   const str = 'Hello';
+//   const newStr = str + ' World';
+
+//   console.log(newStr);
+
+//   функция принимает строку и возвращает ту же строку , но без каждой второй буквы ;
+
+//   const skipString = str => {
+//       let i = 0;
+//       let result = '';
+
+//       while (i < str.length) {   // length = означает количество символов в строке ;
+//         result = result + str[i];  // [] - извлекаем соответствующий символ строки; индексакция  в прошраммировании начинается с 0 ;
+//         i = i + 2;
+//       }
+
+//       return result;
+//   }
+
+// console.log(skipString('California'));
+
+//   const str = 'qwertyuiop';
+//   const result =  `${str[2]}${str[4]}${str[0]}`;
+//   console.log(result);
+
+ 
+
+//    const name ='Alex';  //   интерполяция
+//    const a = 10;
+//    const b = 12;
+
+//    console.log(`His name is ${name} and his age ${a + b}`);
+  
+
+
+  
+ const name = "John" + " Trady";  // конкатенация 
+ console.log(name);
