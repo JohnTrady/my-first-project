4d16e9146ccf965eaa68ebcf8942b360f34abea4
John Trady <docktor.mom19.93@gmail.com>
2022-10-23 21:40:44 +0300

    Разобрали  конструкцию Switch/Case

---
+++ js/switch.js
@@ -0,0 +1,103 @@
+ 'use strict'
+
+
+//  let a = 2 + 2;
+
+//  switch (a) {
+//     case 3:
+//         alert('Wrong! it is small');
+//         break;
+//     case 4: 
+//         alert('Yeah, you get it rigth.');
+//         break;
+//     case 5: 
+//         alert('Oh no. It is a lot.');
+//         break
+        
+//     default: 
+//         alert('it is all wrong. Put another number.')
+//  }
+
+ 
+
+// let a = "1";
+// let b = 0;
+
+// switch (+a) {
+//   case b + 1:
+//     alert("Выполнится, т.к. значением +a будет 1, что в точности равно b+1");
+//     break;
+
+//   default:
+//     alert("Это не выполнится");
+// }
+
+
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+  
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+  
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   }
+
+
+
+// const checkBrowser = browser => {
+//     if(browser === 'Edge') {
+//         return "You've got the Edge!";
+//     } else if(browser ==='Firefox'
+//      || browser ==='Chrome'
+//      || browser == 'Opera'
+//      || browser === 'Safari') {
+//         return   'Okay we support these browsers too';
+//     }
+
+//     else {
+//         return 'We hope that this page looks ok!';
+//     }
+// };
+
+// console.log(checkBrowser('Opera'));
+
+
+// const number = +prompt('Введите число между 0 и 3', '');
+
+// if (number === 0) {
+//   alert('Вы ввели число 0');
+// }
+
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
+
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// }
+
+// const number = +prompt('Введите число между 0 и 3', '');
+
+// switch(number) {
+//     case 0:
+//     alert('Вы ввели число 0');
+//     break;
+//     case 1:
+//         alert('Вы ввели число 1');
+//         break;
+//     case 2:
+//     case 3:
+//         alert('Вы ввели число 2, а может и 3');
+//         break;
+// }       
+
+
+
+
