940f66068b598fa30150856df12a310b44198bc3
John Trady <docktor.mom19.93@gmail.com>
2022-11-06 23:00:18 +0300

    Lesson 5
    Окружение. Замыкания. Чистые функции. Знакомство с массивами.

---
+++ js/array.js
@@ -0,0 +1,35 @@
+'use strict'
+// массивы - упорядоченные наюоры элементов 
+// задача массива - представить такие элементы в виде единной структуры , которая позоляет работать с ними как с единным элементом
+
+// определение массива 
+//  const items = [];
+
+//  создание массива с тремя элементами 
+
+// const animals = ['cats', 'dogs', 'birds'];
+
+//  получение данных 
+
+
+//  console.log(animals[0]); // cats
+//  console.log(animals[1]); // dogs
+//  console.log(animals[2]); // birds
+//  console.log(animals.length); // 3
+
+//  обращенние к элементу при помощи массива 
+
+// const animals = ['cats', 'dogs', 'birds'];
+
+// let i = 1;
+// let j = 1;
+// console.log(animals[i]); // dogs
+// console.log(animals[i + j]); // birds
+
+//  в квадратные скобки  можно подставить вызов функции 
+
+// const getIndexOFSecondElement = () => 1;
+// const animals = ['cats', 'dogs', 'birds'];
+// console.log(animals[getIndexOFSecondElement()]);
+
+
